5ec8d4e0c6c77fd84ce410d14e4a9fec90dc517c
ardolf <ardolf@koncepthk.cz>
2025-01-01 10:42:14 +0100

    Initial commit

---
+++ src/main/java/cz/uhk/fim/kppro/kppro_theroomgameorganizer/KpproTheRoomGameOrganizerApplication.java
@@ -0,0 +1,13 @@
+package cz.uhk.fim.kppro.kppro_theroomgameorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpproTheRoomGameOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpproTheRoomGameOrganizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/uhk/fim/kppro/kppro_theroomgameorganizer/KpproTheRoomGameOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.fim.kppro.kppro_theroomgameorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpproTheRoomGameOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
